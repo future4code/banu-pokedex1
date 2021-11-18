dbfd7472dacd9c087996ebae1053126611b896ec
Juliano Freitas de Souza <rj.jqru@gmail.com>
2021-11-17 23:19:06 -0300

    First changes

--- pokedex/src/pages/Details/index.js
+++ pokedex/src/pages/Details/index.js
@@ -1,9 +1,68 @@
-import {} from './style' // Aqui vai o seu CSS do styled
+import { Container, HeaderSection, HeaderItems, MainSection, GoBackButton, RemoveOrAddButton, DetailsWrapper, FrontImage, BackImage, Stats, Types, Moves, Menu, MenuIcon, MenuHeader, MenuSection, GoBackButtonMenu, RemoveOrAddButtonMenu } from './style'
+
+import { useState } from 'react'
 
 export function Details() {
+
+    const [menuToggle, setMenuToggle] = useState(false)
+
     return (
         <>
-            <h1>Details Page :)</h1>
+            {!menuToggle ? 
+                <Container>
+                    <HeaderSection>
+                        <HeaderItems>
+                            <GoBackButton>Voltar</GoBackButton>
+                            <h3>PokeName</h3>
+                            <RemoveOrAddButton>Adicionar/Remover</RemoveOrAddButton>
+                            <MenuIcon onClick={() => setMenuToggle(!menuToggle)}/>
+                        </HeaderItems>
+                    </HeaderSection>
+                    <MainSection>
+                        <DetailsWrapper>
+                            <FrontImage>
+                                <h4>Frontal Image</h4>
+                            </FrontImage>
+                            <BackImage>
+                                <h4>Back Image</h4>
+                            </BackImage>
+                            <Stats>
+                                <h2>Stats</h2>
+                                <p>HP</p>
+                                <p>Attack</p>
+                                <p>Defense</p>
+                                <p>Special-Attack</p>
+                                <p>Special-Defense</p>
+                                <p>Speed</p>
+                            </Stats>
+                            <Types>
+                                <h2>Type 1</h2>
+                                <h2>Type 2</h2>
+                            </Types>
+                            <Moves>
+                                <h2>Moves</h2>
+                                <p>Move 1</p>
+                                <p>Move 2</p>
+                                <p>Move 3</p>
+                                <p>Move 4</p>
+                            </Moves>
+                        </DetailsWrapper>
+                    </MainSection>
+                </Container>
+                : 
+                <Menu>
+                    <MenuHeader>
+                        <HeaderItems>
+                            <h3>PokeName</h3>
+                            <MenuIcon onClick={() => setMenuToggle(!menuToggle)}/>
+                        </HeaderItems>
+                    </MenuHeader>
+                    <MenuSection>
+                        <GoBackButtonMenu>Voltar</GoBackButtonMenu>
+                        <RemoveOrAddButtonMenu>Adicionar/Remover</RemoveOrAddButtonMenu>
+                    </MenuSection>
+                </Menu>
+            }
         </>
     )
 }
